268e1c98a1e8a958fd93e570df45bf557a5e2114
JoaoRodrigues94 <joaopaulo.fr.1994@gmail.com>
2020-10-15 12:14:05 +0000

    (15/10/2020) - Introdução a orientação a objetos em js.

---
+++ formar-geometricas.js
@@ -0,0 +1,48 @@
+/*
+Área do retângulo: base * altura
+Área do triângulo: base * altura / 2
+Área da elipse: (base / 2) * (altura / 2) * PI
+*/
+
+let retangulo = {
+    base: 4.5,
+    altura: 3.75,
+    tipo: 'R'
+}
+
+let triangulo = {
+    base: 4.5,
+    altura: 3.75,
+    tipo: 'T'
+}
+
+let elipse = {
+    base: 4.5,
+    altura: 3.75,
+    tipo: 'E'
+}
+
+const tchonga = {
+    treco: 'X',
+    gambi: 'arra',
+    tipo: 'T'
+}
+
+const area = forma => {
+    switch(forma.tipo) {
+        case 'R':
+            return forma.base * forma.altura
+        case 'T':
+            return forma.base * forma.altura / 2
+        case 'E': 
+            return (forma.base / 2) * (forma.altura / 2) * Math.PI
+        default:
+            return undefined
+    }
+}
+
+console.log(area(triangulo))
+console.log(area(retangulo))
+console.log(area(elipse))
+console.log(area('teste'))
+console.log(area(tchonga))
