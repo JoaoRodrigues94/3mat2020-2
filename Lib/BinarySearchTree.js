f5230b9c900a2e631c6251167d5e73180a00d8c5
JoaoRodrigues94 <joaopaulo.fr.1994@gmail.com>
2020-11-16 12:08:40 +0000

    16/11 - Início da árvore binária de busca.

---
+++ Lib/BinarySearchTree.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(value){
+        this.data= value
+        this.left = null // Lado esquerdo
+        this.right = null // Lado direito
+    }
+}
+
+module.exports = class BinarySeachTree {
+    constructor(){
+        this.root = null // Raiz da árvore.
+    }
+}
